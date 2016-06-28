55bd5d507da7015f7e1bfcbd1fe311c29b4953ab
Eileen McFarland and Rich Jardine <pair+eileen+rich@allstate.com>
2016-06-28 13:22:58 -0500

    display header on landing page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
 import React from 'react';
 
-export default () => <h1>hello world</h1>;
+class Concert extends React.Component {
+  constructor(props) {
+    super(props);
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.setState();
+  }
+
+  render() {
+    return (<div>
+      <h1>Uptown Concerts! </h1>
+      <h2>Our Venue</h2>
+    </div>
+    );
+  }
+}
+export default Concert;
